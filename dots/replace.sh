2dfc33c26ca11b0cfbb645c2230e01c2e7ddea7e
Sfikas <65178946+SfikasTeo@users.noreply.github.com>
2022-10-08 09:48:03 +0300

    Update replace.sh

--- dots/replace.sh
+++ dots/replace.sh
@@ -1,5 +1,19 @@
 #! /bin/bash
 
+## Run as root 
+
+if [[ $EUID -ne 0 ]]; then
+  echo "You must be super user in order to run this script"
+  exit 1
+fi
+
+## Make the rc files executable
+chmod +x ./bspwm/bspwmrc
+# chmod +x ./sxhkd/sxhkdrc
+
+
+## Place files at the filesystem
+
 cp -r bspwm ~/.config/
 cp -r kitty ~/.config/
 cp -r picom ~/.config/
