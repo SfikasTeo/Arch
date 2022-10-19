5dfb13b06a0484956577629579094f04edcd3efc
Sfikas <65178946+SfikasTeo@users.noreply.github.com>
2022-10-19 11:44:54 +0300

    Update config.fish

--- dots/fish/config.fish
+++ dots/fish/config.fish
@@ -2,8 +2,5 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-# Set Gruvbox Theme
-theme_gruvbox dark hard
-
 # Set Colors to LS
 set LS_COLORS "no=0:rs=0:di=1;34:ex=4;35:fi=0;96:*.c=0;33:*.py=0;32:*.zip=0;33:*.tar=0;33"
