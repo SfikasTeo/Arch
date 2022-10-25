8421f5d7588c7864d7a7a0909d3743b64f35cd98
Sfikas <65178946+SfikasTeo@users.noreply.github.com>
2022-10-25 14:59:21 +0300

    Update fish_prompt.fish

--- dots/fish/functions/fish_prompt.fish
+++ dots/fish/functions/fish_prompt.fish
@@ -1,4 +1,4 @@
 function fish_prompt
-    echo -n (set_color 7588b3)(prompt_pwd)(set_color e77670)' ∮  '
+    echo -n (set_color 7588b3)(prompt_pwd --full-length-dirs=3 --dir-length=3)(set_color e77670)' ∮  '
     set_color normal
 end
