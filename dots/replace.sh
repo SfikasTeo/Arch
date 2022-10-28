425fb5215bcb032fc434e4cc134568665cd5e050
Sfikas <65178946+SfikasTeo@users.noreply.github.com>
2022-10-28 11:14:53 +0300

    Update replace.sh

--- dots/replace.sh
+++ dots/replace.sh
@@ -4,18 +4,6 @@
 chmod +x ./bspwm/bspwmrc
 chmod +x ./polybar/launch.sh
 
-## Configure Fontconfig
-mkdir -p ~/.config/fontconfig
-cp ./fontconfig/fonts.conf ~/.config/fontconfig
-
-# Remove Noto Default Configurations
-rm -rf /etc/fonts/conf.d/66-noto-sans.conf
-rm -rf /etc/fonts/conf.d/66-noto-mono.conf
-rm -rf /etc/fonts/conf.d/66-noto-serif.conf
-
-# Add FiraCode as Default Font
-cp -r ./fontconfig/conf.d/* /etc/fonts/conf.d/
-
 ## Add Wallpaper to ~/Pictures
 mkdir -p ~/Pictures
 cp ../misc/wallpaper.jpg ~/Pictures/
@@ -39,7 +27,3 @@ cp -r ./fish/  ~/.config/
 cp -r ./gtk-3.0/ ~/.config/
 cp -r ./nvim/   ~/.config/
 cp ./X11/.xinitrc ~/
-
-## Add Keyboard Configuration
-cp ./X11/00-keyboard.conf /etc/X11/xorg.conf.d/
-
